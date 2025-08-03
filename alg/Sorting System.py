559bd394d5660612c4f38f16d63cb31a0ea259c3
arian <attaiearian@gmail.com>
2025-08-03 22:05:07 +0330

    Initial Code

---
+++ alg/Sorting System.py
@@ -0,0 +1,40 @@
+import os
+import logging
+
+logging.basicConfig(filename = "Sorting.log", level = logging.INFO , format = '%(filename)s , %(asctime)s , %(levelname)s , %(message)s')
+
+user = os.getlogin()
+files = os.listdir(rf'C:\Users\{user}\Downloads')
+
+for i in files:
+
+    if os.path.isdir(i) == False:
+
+        splited = i.split('.')
+
+        # skipping 'ini' files:
+        if splited[-1] == 'ini':
+            continue
+
+        # sorting system:
+        elif len(splited) > 1:
+
+            # Making directory if it doesn't exist
+            os.makedirs(rf'C:\Users\{user}\Downloads\{splited[-1]}_format', exist_ok=True)
+
+            # Check if the file already exists in the directory or not
+            if os.path.exists(rf'C:\Users\{user}\Downloads\{splited[-1]}_format\{i}') == False:
+                
+                # Moveing the file
+                os.rename(rf'C:\Users\{user}\Downloads\{i}' , rf'C:\Users\{user}\Downloads\{splited[-1]}_format\{i}')
+                logging.info(f'MOVED , file \"{i}\" moved to \"{splited[-1]}_format\" folder ')
+
+            else:
+                logging.info(f'EXIST , file \"{i}\" already exists in \"{splited[-1]}_format\" folder ')
+                continue
+
+        else:
+            continue
+
+    else:
+        continue
