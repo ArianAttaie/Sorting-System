a0409263c20a67767838227d30d4c6d8b0179443
arian <attaiearian@gmail.com>
2025-08-04 22:55:05 +0330

    version 1.0.3 added popup windows

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from alg import Sorting_System
 import tkinter as tk
 from tkinter import ttk
 
-
 def center(win):
     win.update_idletasks()
     width = win.winfo_width()
@@ -23,13 +22,70 @@ def select_path(event):
 
 def select_format(event):
     selected_item = format_combo_box.get()
-    format_label.config(text="Selected format: " + selected_item)
+    format_label.config(text="Selected sorting system: " + selected_item)
 
 def sort_task():
-    Sorting_System.path = path_combo_box.get()
-    Sorting_System.sort_files()
-    print(Sorting_System.path)
+    path = path_combo_box.get()
+    formatting = format_combo_box.get()
+    counter = 0
+    for i in format_list:
+        if i == formatting:
+            counter+=1
 
+    if os.path.isdir(path) == False:
+        top = tk.Toplevel()
+        top.title('Task Report')
+        top.iconbitmap('SortingSystem.ico')
+        top.geometry("150x100")
+        top.resizable(False, False)
+        top.attributes('-alpha', 0.0)
+        center(top)
+        top.attributes('-alpha', 1.0)
+        label = tk.Label(top, text="Directory was not found!")
+        label.pack(pady=10)
+        button = tk.Button(top, text="OK", command=top.destroy)
+        button.pack(pady=10)
+        top.mainloop()
+
+    elif os.path.isdir(path) == True:
+        if counter == 0:
+            top = tk.Toplevel()
+            top.title('Task Report')
+            top.iconbitmap('SortingSystem.ico')
+            top.geometry("300x100")
+            top.resizable(False, False)
+            top.attributes('-alpha', 0.0)
+            center(top)
+            top.attributes('-alpha', 1.0)
+            label = tk.Label(top, text="You have to choose a valid sorting system.")
+            label.pack(pady=10)
+            button = tk.Button(top, text="OK", command=top.destroy)
+            button.pack(pady=10)
+            top.mainloop()
+
+        else:
+            Sorting_System.path = path_combo_box.get()
+            Sorting_System.sort_files()
+            print(Sorting_System.path)
+
+            top = tk.Toplevel()
+            top.title('Task Report')
+            top.iconbitmap('SortingSystem.ico')
+            top.geometry("150x100")
+            top.resizable(False, False)
+            top.attributes('-alpha', 0.0)
+            center(top)
+            top.attributes('-alpha', 1.0)
+            label = tk.Label(top, text="Task compeleted!")
+            label.pack(pady=10)
+            button = tk.Button(top, text="OK", command=top.destroy)
+            button.pack(pady=10)
+            top.mainloop()
+
+        
+
+
+format_list = ["file format"]
 
 root = tk.Tk()
 root.title("Sorting System")
@@ -50,7 +106,7 @@ path_combo_box.pack(pady=5)
 
 format_label = tk.Label(root, text="Choose the sorting system: ")
 format_label.pack(pady=10)
-format_combo_box = ttk.Combobox(root, values=["file format"], width=35)
+format_combo_box = ttk.Combobox(root, values=format_list, width=35)
 format_combo_box.pack(pady=5)
 
 path_combo_box.bind("<<ComboboxSelected>>", select_path)
