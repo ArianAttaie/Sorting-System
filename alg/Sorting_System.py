27f3fffc7474e9a363717214702e860ce4cddf8f
arian <attaiearian@gmail.com>
2025-08-04 22:14:19 +0330

    version 1.0.2 fixed the code and it's working now next is to update the .exe file

--- alg/Sorting_System.py
+++ alg/Sorting_System.py
@@ -2,16 +2,18 @@ import os
 import logging
 
 logging.basicConfig(filename = "Sorting.log", level = logging.INFO , format = '%(filename)s , %(asctime)s , %(levelname)s , %(message)s')
+user = os.getlogin()
+path = str()
 
-def get_user():
-    user = os.getlogin()
-    return user
+def get_list(path):
+    files = os.listdir(path)
+    return files
 
-def sort_files(files_path, user):
-    for i in files_path:
+def sort_files():
+
+    for i in get_list(path):
 
         if os.path.isdir(i) == False:
-
             splited = i.split('.')
 
             # skipping 'ini' files:
@@ -22,18 +24,18 @@ def sort_files(files_path, user):
             elif len(splited) > 1:
 
                 # Making directory if it doesn't exist
-                os.makedirs(rf'C:\Users\{user}\Downloads\{splited[-1]}_format', exist_ok=True)
+                os.makedirs(rf'{path}\{splited[-1]}_format', exist_ok=True)
 
                 # Check if the file already exists in the directory or not
-                if os.path.exists(rf'C:\Users\{user}\Downloads\{splited[-1]}_format\{i}') == False:
+                if os.path.exists(rf'{path}\{splited[-1]}_format\{i}') == False:
                     
                     # Moveing the file
-                    os.rename(rf'C:\Users\{user}\Downloads\{i}' , rf'C:\Users\{user}\Downloads\{splited[-1]}_format\{i}')
+                    os.rename(rf'{path}\{i}' , rf'{path}\{splited[-1]}_format\{i}')
                     logging.info(f'MOVED , file \"{i}\" moved to \"{splited[-1]}_format\" folder ')
 
                 else:
                     logging.info(f'EXIST , file \"{i}\" already exists in \"{splited[-1]}_format\" folder ')
-                    continue
+                continue
 
             else:
                 continue
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import os
 from alg import Sorting_System
 import tkinter as tk
 from tkinter import ttk
 
-user = Sorting_System.get_user()
 
 def center(win):
     win.update_idletasks()
@@ -25,6 +25,11 @@ def select_format(event):
     selected_item = format_combo_box.get()
     format_label.config(text="Selected format: " + selected_item)
 
+def sort_task():
+    Sorting_System.path = path_combo_box.get()
+    Sorting_System.sort_files()
+    print(Sorting_System.path)
+
 
 root = tk.Tk()
 root.title("Sorting System")
@@ -38,22 +43,20 @@ root.attributes('-alpha', 1.0)
 
 path_label = tk.Label(root, text="Choose your folder: ")
 path_label.pack(pady=10)
-path_combo_box = ttk.Combobox(root, values=[rf"C:\Users\{user}\Downloads", rf"C:\Users\{user}\Documents"])
+path_combo_box = ttk.Combobox(root, values=[rf"C:\Users\{os.getlogin()}\Downloads",
+                                             rf"C:\Users\{os.getlogin()}\Documents",
+                                               rf"C:\Users\{os.getlogin()}\OneDrive\Documents"], width=35)
 path_combo_box.pack(pady=5)
 
 format_label = tk.Label(root, text="Choose the sorting system: ")
 format_label.pack(pady=10)
-format_combo_box = ttk.Combobox(root, values=["file format"])
+format_combo_box = ttk.Combobox(root, values=["file format"], width=35)
 format_combo_box.pack(pady=5)
 
-
-path_combo_box.set(rf"C:\Users\{user}\Downloads")
-format_combo_box.set("file format")
 path_combo_box.bind("<<ComboboxSelected>>", select_path)
 format_combo_box.bind("<<ComboboxSelected>>", select_format)
 
-files_path = path_combo_box.get()
-button = tk.Button(root, text="Sort", width=25, command=Sorting_System.sort_files(files_path=files_path, user=user)).pack(pady=15)
+button = tk.Button(root, text="Sort", width=25, command=sort_task).pack(pady=15)
 
 
 root.mainloop()
